d0c9f4941558b6316e9aadf889f846e21337bbe5
Taylor <96550502+taylors42@users.noreply.github.com>
2024-03-13 16:35:04 -0300

    Delete tasks/target directory

--- tasks/target/debug/deps/tasks-834ad377e5129967.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Tay\Documents\GitHub\rust_course_tasks\tasks\target\debug\deps\libtasks-834ad377e5129967.rmeta: src\main.rs
-
-C:\Users\Tay\Documents\GitHub\rust_course_tasks\tasks\target\debug\deps\tasks-834ad377e5129967.d: src\main.rs
-
-src\main.rs:
--- tasks/target/debug/deps/tasks.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Tay\Documents\GitHub\rust_course_tasks\tasks\target\debug\deps\tasks.exe: src\main.rs
-
-C:\Users\Tay\Documents\GitHub\rust_course_tasks\tasks\target\debug\deps\tasks.d: src\main.rs
-
-src\main.rs:
